cfa486a128cc468f0c9ef27635aedaa56db302b4
VictorDevAgencia74 <victor@devagencia.tech>
2025-08-13 10:10:59 -0300

    Excluindo residuos

--- assets/js/js/modals.js
+++
@@ -1,7 +0,0 @@
-// Adicionar evento para atualizar a página ao fechar o modal de adição
-const addModal = document.getElementById("addModalCategoria");
-if (addModal) {
-    addModal.addEventListener("hidden.bs.modal", () => {
-        location.reload(); // Recarrega a página ao fechar o modal
-    });
-}
